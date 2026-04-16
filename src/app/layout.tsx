b05538dfd22be585d9ef9c324f4d9e0852805061
shabeeb <shabeebtk07@gmail.com>
2026-04-16 19:04:53 +0530

    ui alignment fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       <meta name="apple-mobile-web-app-title" content="Goatza" />
-      
+
       <body>
         <QueryProvider>
           <Providers>
